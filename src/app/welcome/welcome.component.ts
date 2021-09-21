279d3552927829c33e19b86f411c3b59dcb70772
sabaja <jacoposabatini76@gmail.com>
2021-09-21 15:38:26 +0200

    add un po di roba

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
+
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
 
-  name = '';
+  appName = '';
+  name = "nobody";
 
-  constructor(private route: ActivatedRoute) { }
+  @Input() header = '';
+  @Input() message = '';
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+
+  /* https://www.guru99.com/angularjs-controller.html */
+  tutoriaName() {
+ 
+    return "Angular JS";
+  }
 
   ngOnInit(): void {
-
+    this.appName = this.tutoriaName();
   }
 
 }
