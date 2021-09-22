523fe49e5f6e7314b79242707a13e6309d133c6b
sabaja <jacoposabatini76@gmail.com>
2021-09-22 13:02:00 +0200

    Modify routing for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const routes: Routes = [
-  { path: "", component: AppComponent },
+  { path: "", component: WelcomeComponent },
   { path: "welcome", component: WelcomeComponent }
 ];
 
